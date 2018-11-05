74fd7164e51c77f930adbad476f474ea8fc99708
LastHero91 <kam.06@mail.ru>
2018-11-05 20:34:28 +0300

    create folder

--- src/main/java/com/k381/Main.java
+++ src/main/java/com/k381/Main.java
@@ -2,17 +2,52 @@ package com.k381;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+    private final static Logger logger = Logger.getLogger("Main");
+
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\Никита\\Projects\\m43\\src\\main\\java\\com\\k381\\Text.txt");
-
-        //fileExpereance(file);
-
+        randomAccessFile();
     }
 
+    private static void randomAccessFile() {
+        File file = new File("C:\\Users\\Никита\\Projects\\m43\\src\\main\\java\\com\\k381\\Text.txt");
+        fileExpereance(file);
+
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file.getPath(), "rw")) {
+            if (!file.exists()) {
+                file.createNewFile();
+            } else {
+                randomAccessFile.setLength(0);
+                randomAccessFile.seek(0);
+            }
+            randomAccessFile.writeUTF(String.valueOf(file.getCanonicalFile().getName()) + ";\n");
+            randomAccessFile.writeInt(1420);
+
+            randomAccessFile.seek(0);
+            System.out.println(randomAccessFile.readUTF());
+            randomAccessFile.seek(randomAccessFile.length() - 4);
+            System.out.println(randomAccessFile.readInt());
+
+        } catch (IOException e) {
+            log(e.getStackTrace(), e.getMessage());
+        }
+    }
+
+    //Печать лога
+    private static void log(StackTraceElement printStackTrace[], String message) {
+        logger.info(message);
+        for (StackTraceElement stackTraceElement : printStackTrace) {
+            logger.log(Level.SEVERE, stackTraceElement.toString());
+        }
+    }
+
+
+    //переписано с лекций
     private static void fileExpereance(File file) {
-        if(file.exists()) {
+        if (file.exists()) {
             try (InputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
                 System.out.println(file.getCanonicalPath());
 
@@ -25,14 +60,14 @@ public class Main {
                     ((dir, name) -> !name.endsWith(".txt"))
             )).stream().forEach(System.out::println);
             System.out.println(file.delete());
-        }else{
+        } else {
             System.out.println("File not found((");
-            try{
+            try {
                 System.out.println(file.createNewFile());
-                System.out.println(file.getFreeSpace()/(1024*1024));
-                System.out.println(file.getTotalSpace()/(1024*1024));
-                System.out.println(file.getUsableSpace()/(1024*1024));
-            }catch(IOException e){
+                System.out.println(file.getFreeSpace() / (1024 * 1024));
+                System.out.println(file.getTotalSpace() / (1024 * 1024));
+                System.out.println(file.getUsableSpace() / (1024 * 1024));
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
